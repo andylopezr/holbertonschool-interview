00d917bf0f7e6e6951215319457b17c0d0e32572
Andy Lopez <profesor.de.ingles@aol.com>
2021-09-02 21:36:35 -0500

    |- ADDED -| missing interview preparation projects

---
+++ 0x01-insert_in_sorted_linked_list/test.c
@@ -0,0 +1,51 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * insert_node - insert node
+ * @head: list
+ * @number: number
+ *
+ * Return: list.
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+    int t = 0;
+    listint_t *new;
+    listint_t *current;
+    current = *head;
+    new = malloc(sizeof(listint_t));
+    if (new == NULL)
+        return (NULL);
+
+    new->n = number;
+    new->next = NULL;
+    if (*head == NULL)
+        *head = new;
+    else
+    {
+        while (current != NULL)
+        {
+            if (current->next != NULL && current->n > number && t == 0)
+            {
+                new->next = current->next;
+                new->n = current->n;
+                current->next = new;
+                current->n = number;
+                current = new->next;
+                t = 1;
+            }
+            current = current->next;
+        }
+        if (t == 0)
+        {
+            current = *head;
+            while (current->next != NULL)
+                current = current->next;
+            current->next = new;
+        }
+    }
+    return (new);
+}
